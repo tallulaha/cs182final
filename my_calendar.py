258dd82aaedc2723809a1b220a7c65104f9b77e3
lula.simone@yahoo.com <tallulah@Tallulahs-MacBook-Air.local>
2017-12-04 00:00:47 -0500

    added free time

--- my_calendar.py
+++ my_calendar.py
@@ -10,6 +10,7 @@ from oauth2client.file import Storage
 
 import datetime
 import dateutil.parser
+import calendar
 
 try:
     import argparse
@@ -23,7 +24,7 @@ SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
-sleep = {'wakeup': '8AM', 'bedtime': '12AM'}
+sleep = {'wakeup': '08:00:00', 'bedtime': '23:59:59'}
 time_preferences = ['morning', 'afternoon', 'evening']
 workout_goals = ['lose weight', 'gain muscle', 'increase endurance', 'new skills']
 
@@ -91,40 +92,104 @@ def main():
     print('Getting the upcoming weeks events')
     # need to think about if there are multiple calendars 
     eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, timeMax=stopdate, singleEvents=True,
-        orderBy='startTime').execute()
+       calendarId='primary', timeMin=now, timeMax=stopdate, singleEvents=True,
+       orderBy='startTime').execute()
     events = eventsResult.get('items', [])
+    #
+    # events = []
+
+    # page_token = None
+    # while True:
+    #   calendar_list = service.calendarList().list(pageToken=page_token).execute()
+    #   for calendar_list_entry in calendar_list['items']:
+    #     #print (calendar_list_entry['summary'])
+    #     eventsResult = service.events().list(
+    #         calendarId=calendar_list_entry['summary'], timeMin=now, timeMax=stopdate, singleEvents=True,
+    #         orderBy='startTime').execute()
+    #     events = eventsResult.get('items', events)
+    #   page_token = calendar_list.get('nextPageToken')
+    #   if not page_token:
+    #     break
+
+    conflict_dict = {}
+    last_end = sleep['wakeup']
+    ed = None
+    et = None
 
     if not events:
         print('No upcoming events found.')
     for event in events:
+        #print(event)
         start = event['start'].get('dateTime', event['start'].get('date'))
+        day, time = start.split("T")
+        # want to get free time until bedtime
+        # this isnt right yet
+        # print (ed)
+        # print (day)
+        # print (ed == day)
+        # print (et < sleep['bedtime'])
+        if et != None and ed != None:
+            if (ed != day) and (et < sleep['bedtime']):
+                print ("hello")
+                breakTime(et, sleep['bedtime']+'-05:00', ed, conflict_dict)
+        if day not in conflict_dict:
+            conflict_dict[day] = []
+            wakeup_format = day + 'T' + sleep['wakeup']
+            if start > wakeup_format:
+                breakTime(sleep['wakeup']+ '-05:00', time, day, conflict_dict)
+        else:
+            if (last_end < start) and (ed == day):
+                d,t = last_end.split("T")
+                breakTime(t,time,d,conflict_dict)
+        # need to check bedtime to make sure it doesnt go over
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        ed, et = end.split("T")
         print(start, event['summary'])
+        # just want the end time !!
+        last_end = end
+        print (conflict_dict)
 
-def createEvent(day, time, descrip, loc):
-    event = {}
-    event = {
-        'summary': descrip[0],
-        'location': loc,
-        'description': descrip[1],
-        'start': {
-            'dateTime': time[0],
-            'timeZone': 'America/New_York',
-        },
-        'end': {
-            'dateTime': '2015-05-28T17:00:00-07:00',
-            'timeZone': 'America/New_York',
-        },
-        'reminders':{
-            'useDefault': True,
-        }
-    }
-    return event
+        # {(12-03, Sunday) : [(8,10), (10-12), (12, 1)]; (12-04, Monday): [(8,10), (10-12), (12, 1)]}
+        # want to split 8-3 into 8-11:59, 12-3
+        # calendar.day_name[my_date.weekday()]
+
+def breakTime(start,end,day,dic):
+    if (end >= '12:00:00') and (start < '12:00:00'):
+        dic[day].append((start, '11:59:59-05:00'))
+        if end >= '17:00:00':
+            dic[day].append(('12:00:00-05:00', '16:59:59-05:00'))
+        else:
+            dic[day].append(('12:00:00-05:00', end))
+    elif (end >='17:00:00') and (start < '17:00:00'):
+        dic[day].append((start, '16:59:59-05:00'))
+        dic[day].append(('17:00:00-05:00', end))
+    else:
+        dic[day].append((start, end))
+
+# def createEvent(day, time, descrip, loc):
+#     event = {}
+#     event = {
+#         'summary': descrip[0],
+#         'location': loc,
+#         'description': descrip[1],
+#         'start': {
+#             'dateTime': time[0],
+#             'timeZone': 'America/New_York',
+#         },
+#         'end': {
+#             'dateTime': '2015-05-28T17:00:00-07:00',
+#             'timeZone': 'America/New_York',
+#         },
+#         'reminders':{
+#             'useDefault': True,
+#         }
+#     }
+#     return event
 
 
-def addWorkout(event):
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print 'Event created: %s' % (event.get('htmlLink'))
+# def addWorkout(event):
+#     event = service.events().insert(calendarId='primary', body=event).execute()
+#     print 'Event created: %s' % (event.get('htmlLink'))
 
 if __name__ == '__main__':
     main()
